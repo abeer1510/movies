49fa8ffc6052c84033fda7e58b937a5c2c331801
abeer1510 <abeer1510.m@gmail.com>
2025-02-11 23:00:13 +0200

    home tap

--- lib/api_manager.dart
+++ lib/api_manager.dart
@@ -3,16 +3,25 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:news/model/sources_response.dart';
 
+import 'model/upcoming_response.dart';
+
 class ApiManager{
 
   static Future<SourcesResponse> getPopular()async{
-    Uri url=Uri.https("api.themoviedb.org/","/3/tv/popular",{
-      "apiKey":"1af5751239f6c52b196a77e23dcf8416"}
-    );
+    Uri url=Uri.parse("https://api.themoviedb.org/3/tv/popular?api_key=1af5751239f6c52b196a77e23dcf8416");
 
     http.Response response = await http.get(url);
     var json =jsonDecode(response.body);
     SourcesResponse sourcesResponse=SourcesResponse.fromJson(json);
     return sourcesResponse;
+  }
+
+  static Future<UpcomingResponse> getupComing()async{
+    Uri url=Uri.parse("https://api.themoviedb.org/3/movie/upcoming?api_key=1af5751239f6c52b196a77e23dcf8416");
+
+    http.Response response = await http.get(url);
+    var json =jsonDecode(response.body);
+    UpcomingResponse upcomingResponse=UpcomingResponse.fromJson(json);
+    return upcomingResponse;
   }
 }
--- lib/screens/tabs/home_tab.dart
+++ lib/screens/tabs/home_tab.dart
@@ -1,7 +1,11 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:news/api_manager.dart';
 import 'package:news/items/movie_item.dart';
+import 'package:news/items/upcoming_item.dart';
 import 'package:news/model/sources_response.dart';
+
+import '../../model/upcoming_response.dart';
 
 class HomeTab extends StatelessWidget {
   const HomeTab({super.key});
@@ -9,8 +13,7 @@ class HomeTab extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: FutureBuilder<SourcesResponse>
-        (future: ApiManager.getPopular(),
+      body: FutureBuilder<SourcesResponse>(future: ApiManager.getPopular(),
           builder: (context,snapshot){
           if(snapshot.connectionState == ConnectionState.waiting){
             return Center(child: CircularProgressIndicator());
@@ -19,19 +22,51 @@ class HomeTab extends StatelessWidget {
             return Center(child: Text("Something Went Wrong",style: Theme.of(context).textTheme.titleLarge,));
           }
           var data = snapshot.data?.results??[];
-          return DefaultTabController(
-            length: data.length,
-            child: Column(
-              children: [
-                Expanded(
-                  child: ListView.separated(
-                    scrollDirection: Axis.horizontal,
-                      itemBuilder: (context,index){
-                   return MovieItem(results: snapshot.data!.results![index]);
-                  }, separatorBuilder: (context,index)=>SizedBox(width: 10,), itemCount: snapshot.data?.results?.length??0),
-                ),
-              ],
-            ),
+          return Stack(
+            children: [
+              Image(image: AssetImage("assets/images/background.png"),height: double.infinity,width: double.infinity,fit: BoxFit.fill,),
+              Column(
+                children: [
+                  Image(image: AssetImage("assets/images/available.png")),
+                  Stack(
+                    alignment: Alignment.bottomCenter,
+                    children: [
+                      CarouselSlider.builder(
+                        itemBuilder: (context,index,realIndex){
+                            return MovieItem(results: snapshot.data!.results![index]);
+                        },
+                        itemCount:snapshot.data?.results?.length??0,
+                        options: CarouselOptions(
+                          height: 440,
+                          autoPlay: false,
+                        enlargeCenterPage: true,
+                        enableInfiniteScroll: false,
+                        scrollDirection: Axis.horizontal,
+                        viewportFraction: 0.55,
+                          aspectRatio: 16 / 9,
+                        ),),
+                      Image(image: AssetImage("assets/images/watch.png")),
+                    ],
+                  ),
+                  FutureBuilder(future: ApiManager.getupComing(), builder: (context,snapshot){
+                    return ListView.separated(itemBuilder: (context,index){
+                      if(snapshot.connectionState == ConnectionState.waiting){
+                        return Center(child: CircularProgressIndicator());
+                      }
+                      if(snapshot.hasError){
+                        return Center(child: Text("Something Went Wrong",style: Theme.of(context).textTheme.titleLarge,));
+                      }
+                      var data = snapshot.data?.results??[];
+                      return UpcomingItem(results: snapshot.data!.results![index]) ;
+                    },itemCount:snapshot.data?.results?.length,
+
+                      separatorBuilder: (context,index)=>SizedBox(width: 10,),);
+                  })
+
+                ],
+              ),
+
+            ],
           );
           }),
     );
