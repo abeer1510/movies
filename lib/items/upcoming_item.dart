54a2a692df3cb5be8d97c4cac97ef5a464204f6c
Abanoub wahied <gfx2day@gmail.com>
2025-02-13 11:28:22 +0200

    remove result class from upcoming model & auto log in

--- lib/items/upcoming_item.dart
+++ lib/items/upcoming_item.dart
@@ -2,11 +2,11 @@ import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:news/model/sources_response.dart';
-import 'package:news/model/upcoming_response.dart';
-import 'package:news/screens/details_screen.dart';
+
+import '../screens/details_screen.dart';
 
 class UpcomingItem extends StatelessWidget {
-  Results1 results;
+  Results results;
   UpcomingItem({super.key,required this.results});
   @override
   Widget build(BuildContext context) {
@@ -14,21 +14,22 @@ class UpcomingItem extends StatelessWidget {
       children: [
         ClipRRect(
             borderRadius: BorderRadius.circular(20),
-            child: Stack(
+            child:
+            Stack(
               children: [
                 GestureDetector(
-                  onTap: (){
-                    Navigator.pushNamed(context, DetailsScreen.routeName);
-                  },
+                    onTap: (){
+                      Navigator.pushNamed(context, DetailsScreen.routeName);
+                    },
                     child: Image.network("https://image.tmdb.org/t/p/w500${results.posterPath}"??"",fit: BoxFit.fill,height: 220,width: 150,)),
                 Padding(
                   padding: const EdgeInsets.only(top: 13,left: 14),
                   child: Container(
-                    padding: EdgeInsets.symmetric(horizontal: 7),
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10),
-                      color: Color(0xB5121312),
-                    ),
+                      padding: EdgeInsets.symmetric(horizontal: 7),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        color: Color(0xB5121312),
+                      ),
                       child: Row(
                         children: [
                           Text("${results.voteCount}",style: Theme.of(context).textTheme.titleSmall),
@@ -37,8 +38,7 @@ class UpcomingItem extends StatelessWidget {
                       )),
                 ),
               ],
-            ))
-      ],
+            ))      ],
     );
   }
 }
